e37657a73ef8ed5640504f134e8c024c8709ad0e
Gijs van de Nieuwegiessen <gijs@flow.ai>
2017-06-17 21:07:43 +0200

    Added custom response template

---
+++ FlowCore/Classes/CustomTemplate.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+/**
+    Custom response template
+    Use this to create custom UI elements like product listings, pages or widgets
+ */
+public class CustomTemplate : Template {
+    
+    /// Custom data
+    private(set) public var data: [String: Any]!
+    
+    override init(_ data: [String: Any]) throws {
+        try super.init(data)
+        
+        var filteredData = data
+        filteredData.removeValue(forKey: "quickReplies")
+        
+        self.data = filteredData
+    }
+
+    public required init() {
+        super.init()
+    }
+}
